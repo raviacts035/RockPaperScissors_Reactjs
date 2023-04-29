56faccafcded998e3ff778df3a266f933c37d3b1
raviacts035 <ravindrainturi035@gmail.com>
2023-04-29 18:49:03 +0530

    Update CSS :CSS and small fixes

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom/client";
 
 //Checking New click clicked
+const Paper=require("./assets/Paper.jpg")
+const Rock=require("./assets/Rock.jpg")
+const Scissor=require("./assets/Scissor.jpg")
+
 
 const Body=()=>{
-    const [userPiK,setUserPik]=useState("")
-    const [sysPik,setSysPik]=useState("")
+    const [userPiK,setUserPik]=useState(null)
+    const [sysPik,setSysPik]=useState(null)
     const [userWon, setUserWon]=useState(false)
     const [drawMatch,setDrawMatch]=useState(false)
     const dList=["Rock","Paper","Scissor"];
+    const [userPoint,setUserPoints]=useState(0)
+    const [sysPoint,setSysPoints]=useState(0)
+
     
     function sysgen(){
         let x=dList[Math.floor(Math.random()*3)]
@@ -19,34 +25,45 @@ const Body=()=>{
         setUserPik(input)
         setDrawMatch(false)
         setUserWon(false)
-        sysgen()
+        setTimeout(sysgen(),6000);
+        //Draw Match
         if(userPiK==sysPik){
             setDrawMatch(true)
             console.log()
             return
         }
+        let x=whoWon(userPiK,sysPik)
         setUserWon(whoWon(userPiK,sysPik))
+        if(x) setUserPoints(userPoint+1)
     }
-
     return(
         <>
-            <h2>This is some</h2>
+            <h2 className="wel">Welcome to the Game...</h2>
             <div className="space">
                 <div className="cardSpace" id="userCard">
-                    {userPiK?<Card val={userPiK} />:<div></div>}
-                </div>
-                <div className="dLine"></div>
+                    <div>
+                    <h2>User</h2>
+                    <span>Points : {userPoint}</span>
+                    </div>
+                    <img src={cardPick(userPiK)}/>
+                </div> 
+                {/* <div className="dLine"><img src={require("./assets/umpire.jpg")}/></div> */}
+            {(userWon)?<ResultUser/>:drawMatch?<ResultDraw/>:<ResultSystem/>}
                 <div className="cardSpace" id="sysCard">
-                {sysPik?<Card val={sysPik} />:<div></div>}
+                <div>
+                    <h2>System</h2>
+                    <span>Points : {sysPoint}</span>
+                </div>
+                <img src={cardPick(sysPik)}/>
                 </div>
             </div>
-            {userWon?<ResultUser/>:drawMatch?<ResultDraw/>:<ResultSystem/>}
+        {/* <hr></hr> */}
         <div className="btn-grup">
-            <button onClick={()=>{action("Rock")}} id="rock">Rock</button>
-            <button onClick={()=>{action("Paper")}}id="paper">Paper</button>
-            <button onClick={()=>{action("Scissor")}} id="Scissor">Scissor</button>
+            <button onClick={()=>{action("Rock")}} id="rock"><img src={require("./assets/rock_btn.jpg")}/></button>
+            <button onClick={()=>{action("Paper")}}id="paper"><img src={require("./assets/paper_btn.jpg")}/></button>
+            <button onClick={()=>{action("Scissor")}} id="Scissor"><img src={require("./assets/Scissor_btn.jpg")}/></button>
         </div>
-        <h3>Pick Your Choice</h3>
+            <div>Click on Your Choice</div>
         </>
     )
 }
@@ -54,10 +71,13 @@ const Body=()=>{
 
 //cards Model
 const Card=(props)=>{
+    let url="./assets/"+props.val+".jpg"
+    
     return(
         <>
         <div className="card">
             <h2>{props.val}</h2>
+            <img src="assets/Roc.jpg"/>
         </div>
         </>
     )
@@ -65,8 +85,7 @@ const Card=(props)=>{
 const ResultUser=()=>{
     return (<>
     <div className="resultUser">
-    <h2>Congractulations, You Won </h2>
-    <p>Play Again : </p>
+    <h2>You Won : {`)`}</h2>
     </div>
     </>)
 }
@@ -74,15 +93,13 @@ const ResultSystem=()=>{
     return (<>
     <div className="resultUser">
     <h2>Oops, You Lost </h2>
-    <p>Try Again</p>
     </div>
     </>)
 }
 const ResultDraw=()=>{
     return (<>
     <div className="resultUser">
-    <h2>Lol, It's a Draw Match</h2>
-    <p>No worries</p>
+    <h2>Draw Match</h2>
     </div>
     </>)
 }
@@ -96,6 +113,21 @@ function whoWon(userPiK,sysPik){
     }
     else {
         return false
+    }
+}
+
+function cardPick(x){
+    if (!x){
+        return
+    }
+    if (x=="Rock"){
+        return Rock
+    }
+    else if(x=="Paper"){
+        return Paper
+    }
+    else{
+        return Scissor
     }
 }
 
